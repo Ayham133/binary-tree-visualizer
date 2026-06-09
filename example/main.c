0f50a3810eb0f8f273fc6ecda971a51e1a5f0982
Ayham133 <ayhamatallah52@gmail.com>
2026-06-09 18:35:58 +0300

    prototype for rendering the root node and it's data in the center of the rendered circle.

--- example/main.c
+++ example/main.c
@@ -16,7 +16,8 @@ int main(void)
 
     int window_width = 800;
     int window_height = 450;
-   Binary_tree *tree = init_tree(10, window_width/2, 100);
+    Node *root = init_node(10, window_height/2, 100);
+   Binary_tree *tree = init_tree_node(root);
 
    Node *node = init_node(15, (window_width/2 - 100), window_height/2 - 100);
    Node *node2 = init_node(5, (window_width/2 - 100), window_height/2 - 100);
@@ -26,17 +27,25 @@ int main(void)
    pre_order_traversal((tree->head));
    printf("\n");
 
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(window_width, window_height, "Binary Tree");
 
+   while(!WindowShouldClose())
+   {
+       BeginDrawing();
+       ClearBackground(RAYWHITE);
 
-//    while(!WindowShouldClose())
-//    {
-//        BeginDrawing();
-//        ClearBackground(RAYWHITE);
+       // Drawing the root node of tree.
+       DrawCircleLinesV(tree->head->center, 50.0f, BLACK);
+       int text_width = MeasureText(TextFormat("%d", tree->head->data), 20);
+       int text_x = (int)(tree->head->center.x - text_width / 2.0f);
+       int text_y = (int)(tree->head->center.y - 20 / 2.0f);
+       DrawText(TextFormat("%d", tree->head->data), text_x, text_y, 20, BLACK);
 
-//        EndDrawing();
-//    }
+       EndDrawing();
+   }
 
-//    CloseWindow();
+   CloseWindow();
 
     return EXIT_SUCCESS;
 }
